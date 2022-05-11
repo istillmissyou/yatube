4babe86fd93fa9fffde9344c6817abc30cc2c218
Данил Штунь <danilshtun@yandex.ru>
2022-05-11 22:25:29 +0500

    iter1

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -1,4 +1,5 @@
 from linecache import cache
+
 from django import forms
 from django.conf import settings
 from django.contrib.auth import get_user_model
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
-
 handler404 = 'core.views.page_not_found'
 handler500 = 'core.views.server_error'
 handler403 = 'core.views.permission_denied'
